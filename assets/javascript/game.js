162ce259846ba33d8b38a76e6207136eae7cff2d
Luke Skywalker <joshua.mobley.jm@gmail.com>
2018-11-16 15:46:15 -0700

    Current Word now saving properly and added begining of logic for guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,3 @@
-// Writes the letter of the key pressed to the screen, only accepts letters
-// Creates current word from list and populates to page
 var storedLetters = [];
 var SongList = ['bandito', 'carradio', 'blurryface', 'chlorine', 'trees'];
 var rand = SongList[Math.floor(Math.random() * SongList.length)];
@@ -8,33 +6,56 @@ RandArray = rand.split("");
 addEventListener
 
 var Game = {
-    RecordKey: document.onkeyup = function KeyPress(e) {
+    // Writes the letter of the key pressed to the screen, only accepts letters
+    // Creates current word from list and populates to page]
+    NewCurrent: [],
+    Guesses: 15,
+    KeyPress: function (e) {
         var letter = e.key.toLocaleLowerCase();
         var options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
         if (options.indexOf(letter) !== -1) {
             storedLetters.push(letter.toString());
             document.getElementById('GuessLetters').innerHTML = storedLetters;
         }
-        // Replaces the characters of the current words with '_'
-        function Current() {
-            var NewCurrent = rand.split("");
-            for (var i = 0; i < RandArray.length; i++) {
-                NewCurrent[i] = '_';
-                document.getElementById('CurrentWord').innerHTML = NewCurrent;
-            }
-            // if randarray (array of letters in word) is not equal to -1 (means its present)
-            // replace the letter in Current Word
-            function Check() {
-                var Replace = RandArray.indexOf(letter);
-                for (var i = 0; i < RandArray.length; i++) {
-                    if (letter === RandArray[i]) {
-                        NewCurrent[Replace] = letter;
-                    }
-                    document.getElementById('CurrentWord').innerHTML = NewCurrent;
-                }
-            }
-            Check();
+    },
+
+    // Replaces the characters of the current words with '_'
+    Current: function () {
+        for (var i = 0; i < RandArray.length; i++) {
+            Game.NewCurrent[i] = '_';
         }
-        Current();
+    },
+    Check: function (e) {
+        // Checks for a match between the key press and current word that is hidden, and changes current word for all letters that match
+        for (var i = 0; i < RandArray.length; i++) {
+            var letter = e.key.toLocaleLowerCase();
+            if (letter === RandArray[i]) {
+                Game.NewCurrent[i] = letter;
+                document.getElementById('CurrentWord').innerHTML = Game.NewCurrent;
+            }
+        }
+    },
+    SubtractGuess: function() {
+        var letter = e.key.toLocaleLowerCase();
+        if (letter !== RandArray) {
+            console.log(Game.letter);
+        }
     }
 }
+Game.Current();
+// Calls the function to store the Key strokes into Guessed Letters Section
+document.onkeyup = function (e) {
+    Game.KeyPress(e);
+    Game.Check(e);
+}
+
+// Calls Check Function to verifiy if key is present in word
+// document.onkeypress = function (e) {
+    
+// }
+// Loads the Underscores for the Current Word section of the screen
+window.onload = function () {
+    document.getElementById('CurrentWord').innerHTML = Game.NewCurrent;
+    document.getElementById('Remaining').innerHTML = Game.Guesses;
+}
+
